9e4887a2f27e38d16e6804bd35c7340415d320f7
graemedavidson <graeme.davidson@capablue.com>
2015-01-23 11:23:58 +0000

    reorganise vars

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,23 +1,7 @@
-define xdebug::config (
-  $service                = undef,
-  $ini_file_path          = undef,
-  $default_enable         = undef,
-  $remote_enable          = undef,
-  $remove_connect_bacl    = undef,
-  $remote_port            = undef,
-  $remote_handler         = undef,
-  $remote_host            = undef,
-  $remote_autostart       = undef,
-  $show_exception_trace   = undef,
-  $show_local_vars        = undef,
-  $var_display_max_data   = undef,
-  $var_display_max_depth  = undef,
-  $profiler_output_dir    = undef,
-  $idekey                 = undef,
-  $max_nesting_level      = undef
+class xdebug::config (
 ) {
   file { "$ini_file_path" :
-    content => template('xdebug/ini.erb'),
+    content => template('xdebug/templates/ini.erb'),
     ensure  => present,
     require => Package['xdebug'],
     notify  => Service[$service],
--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,7 @@
 # Install and configure xdebug
 #
 # === Parameters
+#  [*service*]                - service to notify (default '')
 #  [*ini_file_path*]          - ini file path (default '')
 #  [*default_enable*]         - default enable xdebug (default '')
 #  [*remote_enable*]          - allow remote connections (default '')
@@ -26,20 +27,20 @@
 class xdebug (
   $service                = $xdebug::params::service,
   $ini_file_path          = $xdebug::params::ini_file_path,
-  $default_enable         = $xdebug::params::default_enable,
   $remote_enable          = $xdebug::params::remote_enable,
-  $remove_connect_bacl    = $xdebug::params::remote_enable,
   $remote_port            = $xdebug::params::remove_connect_back,
-  $remote_handler         = $xdebug::params::remote_port,
   $remote_host            = $xdebug::params::remote_handler,
+  $remote_handler         = $xdebug::params::remote_port,
   $remote_autostart       = $xdebug::params::remote_host,    
-  $show_exception_trace   = $xdebug::params::remote_autostart,
+  $remote_connect_back    = $xdebug::params::remote_enable,
+  $default_enable         = $xdebug::params::default_enable,
   $show_local_vars        = $xdebug::params::show_local_vars,  
   $var_display_max_data   = $xdebug::params::var_display_max_data,
   $var_display_max_depth  = $xdebug::params::var_display_max_depth,
+  $show_exception_trace   = $xdebug::params::remote_autostart,
+  $max_nesting_level      = $xdebug::params::max_nesting_level
   $profiler_output_dir    = $xdebug::params::profiler_output_dir,
   $idekey                 = $xdebug::params::idekey,
-  $max_nesting_level      = $xdebug::params::max_nesting_level
 ) inherits xdebug::params {
   class { 'xdebug::install': }
   class { 'xdebug::config': }
