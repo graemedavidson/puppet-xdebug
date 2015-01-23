406a079ac3ca1188cdf1f1c0563b16b37dd64820
graemedavidson <graeme.davidson@capablue.com>
2015-01-23 13:39:19 +0000

    final update

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,9 +1,9 @@
 class xdebug::config (
 ) {
-  file { "$ini_file_path" :
+  file { "$xdebug::ini_file_path" :
     content => template('xdebug/ini.erb'),
     ensure  => present,
     require => Package[$xdebug::params::pkg],
-    notify  => Service[$service],
+    notify  => Service[$xdebug::params::service],
   }
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,7 @@
 class xdebug::install {
-  if defined(Package[$method_package]) == false {
-    package { $xdebug::params::pkg:
-      ensure  => installed,
-      require => Package[$xdebug::params::php],
-      notify  => Service[$service],
-    }
+  package { $xdebug::params::pkg:
+    ensure  => installed,
+    require => Package[$xdebug::params::php],
+    notify  => Service[$xdebug::params::service],
   }
 }
